db9c57d6ad807ec39f550c1add9172e2884526ca
Grzegorz Konert <konert.grzegorz@gmail.com>
2019-09-10 22:38:43 +0200

    Updates

---
+++ precache-manifest.dbc58b93192cb76c75fef187d9012b58.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5f683fcde4a4251f45be36d64b7b0a5",
+    "url": "/ToDo/index.html"
+  },
+  {
+    "revision": "ecd0b4c2b19949525fe0",
+    "url": "/ToDo/static/css/main.01657c6f.chunk.css"
+  },
+  {
+    "revision": "84714a95a3d225b66f3e",
+    "url": "/ToDo/static/js/2.f36439ab.chunk.js"
+  },
+  {
+    "revision": "ecd0b4c2b19949525fe0",
+    "url": "/ToDo/static/js/main.55e630dd.chunk.js"
+  },
+  {
+    "revision": "2bd618016c368a4069d0",
+    "url": "/ToDo/static/js/runtime~main.d732d018.js"
+  }
+]);
